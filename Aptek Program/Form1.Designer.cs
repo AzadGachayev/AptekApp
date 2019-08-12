aaeef735e098737587365d16e3c1e28b47d462da
AzadGachayev <azad.gachayev@gmail.com>
2019-08-13 03:25:00 +0400

    AptekTest

---
+++ Aptek Program/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace Aptek_Program
+{
+    partial class SAF
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.lblError = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.lblWelcome = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.Purple;
+            this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.ForeColor = System.Drawing.Color.White;
+            this.btnLogin.Location = new System.Drawing.Point(43, 252);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(96, 35);
+            this.btnLogin.TabIndex = 13;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Click += new System.EventHandler(this.BtnLogin_Click);
+            // 
+            // lblError
+            // 
+            this.lblError.AutoSize = true;
+            this.lblError.BackColor = System.Drawing.Color.Purple;
+            this.lblError.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblError.ForeColor = System.Drawing.Color.White;
+            this.lblError.Location = new System.Drawing.Point(39, 327);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(49, 20);
+            this.lblError.TabIndex = 12;
+            this.lblError.Text = "Error";
+            this.lblError.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Purple;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(39, 178);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 20);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Password";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(43, 214);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(100, 20);
+            this.txtPassword.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Purple;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(39, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 20);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Username";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(43, 129);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(100, 20);
+            this.txtUsername.TabIndex = 8;
+            // 
+            // lblWelcome
+            // 
+            this.lblWelcome.AutoSize = true;
+            this.lblWelcome.BackColor = System.Drawing.Color.Transparent;
+            this.lblWelcome.Font = new System.Drawing.Font("Times New Roman", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWelcome.ForeColor = System.Drawing.Color.Purple;
+            this.lblWelcome.Location = new System.Drawing.Point(452, 9);
+            this.lblWelcome.Name = "lblWelcome";
+            this.lblWelcome.Size = new System.Drawing.Size(115, 31);
+            this.lblWelcome.TabIndex = 14;
+            this.lblWelcome.Text = "Welcome";
+            // 
+            // SAF
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Fuchsia;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblWelcome);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.lblError);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtUsername);
+            this.Name = "SAF";
+            this.Text = "Aptek";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.SAF_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label lblError;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Label lblWelcome;
+    }
+}
+
---
+++ Aptek Program/Form1.cs
@@ -0,0 +1,81 @@
+﻿using Aptek_Program.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aptek_Program
+{
+    public partial class SAF : Form
+    {
+        AptekApp DB = new AptekApp();
+        public SAF()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void SAF_Load(object sender, EventArgs e)
+        {
+            lblWelcome.Text = "Xoş gəlmisiniz!";
+            Settings selectedSetting = DB.Settings.First();
+            this.Icon = new Icon("../../Uploads/" + DB.Settings.First().Logo);
+            this.Text = selectedSetting.Name;
+            lblWelcome.Text = selectedSetting.Header;
+        }
+        private void BtnLogin_Click(object sender, EventArgs e)
+        {
+            string email = txtUsername.Text;
+            string password = txtPassword.Text;
+            if (email != null && password != null)
+            {
+                Admin selectAdmin = DB.Admin.FirstOrDefault(ad => ad.Email == email);
+                if (selectAdmin != null)
+                {
+                    if (selectAdmin.Password == password)
+                    {
+                        AdminDashboard Adm = new AdminDashboard();
+                        Adm.ShowDialog();
+                    }
+
+                }
+                else
+                {
+                    Workers selectWorker = DB.Workers.FirstOrDefault(wrk => wrk.Username == email);
+                    if (selectWorker != null)
+                    {
+                        if (selectWorker.Password != password)
+                        {
+                            WorkersDashboard wd = new WorkersDashboard();
+                            wd.ShowDialog();
+                        }
+                        else
+                        {
+                            lblError.Text = "Shifre duzgun deyil!";
+                            lblError.Visible = true;
+                        }
+                    }
+                    else
+                    {
+                        lblError.Text = "Email duzgun deyil!";
+                        lblError.Visible = true;
+                    }
+                }
+
+            }
+            else
+            {
+                lblError.Text = "Zehmet olmasa xanalari doldurun!";
+                lblError.Visible = true;
+            }
+        }
+    }
+}
+
+                  
